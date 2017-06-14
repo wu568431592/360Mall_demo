77cb7f1833950fa89c30716c3cefd59b9883c4ce
wu568431592 <wuwenchao0314@163.com>
2017-06-14 11:40:21 +0800

    1`

---
+++ src/components/black-header/black-header.vue
@@ -0,0 +1,124 @@
+<template>
+    <div id="black-header">
+      <div class="header_box">
+        <div class="go_back_box">
+
+        </div>
+        <div class="header_name">{{word}}</div>
+        <div class="bread_box" @click="show_bottom">
+          <svg viewBox="0 0 30 21">
+            <path d="M0,0v3h30V0H0z M0,12h30V9H0V12z M0,21h30v-3H0V21z"></path>
+          </svg>
+        </div>
+        <div class="header_do" v-show="isshowHeader_do">编辑</div>
+      </div>
+      <div class="header_bottom" v-show="isbottomshow">
+        <div class="header_bottom_item">
+          首页
+        </div>
+        <div class="header_bottom_item">
+          购物车
+        </div>
+        <div class="header_bottom_item">
+          我的订单
+        </div>
+      </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'black-header',
+        data(){
+            return {
+              isbottomshow:false
+            }
+        },
+        props:['showHeader_do','word'],
+        computed:{
+          isshowHeader_do:function(){
+            if(this.showHeader_do == 'true'){
+                return true
+            }
+            return false
+          }
+        },
+        methods:{
+          show_bottom:function(){
+            this.isbottomshow = !this.isbottomshow
+          }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+  #black-header{
+    width:100%;
+    text-align:center;
+    position:relative;
+    div.header_box{
+      height:43px;
+      background:#000000;
+      color:#fff;
+      padding:0.2rem 0.4rem;
+      line-height:36px;
+      overflow: hidden;
+      div{
+        float:left;
+      }
+      div:last-child{
+        float:right
+      }
+      div.go_back_box{
+        width:25px;
+        height:25px;
+        margin-top:6px;
+        background: url(../../assets/images/t014c7a9a806382db2a.png) no-repeat center;
+        background-size: 40%;
+      }
+      .bread_box{
+        float:right;
+      }
+      .header_name{
+        position:absolute;
+        top:0;
+        left:0;
+        line-height: 43px;
+        width:100%;
+        font-size:90%;
+      }
+      .header_do{
+        font-size:90%;
+
+      }
+      .bread_box{
+        position:absolute;
+        top:3px;
+        right:7px;
+        z-index: 10000;
+        color:#fff;
+        margin-top:2px;
+        margin-left:10px;
+        width:20px;
+        height:30px;
+        svg{
+          fill: #fff;
+        }
+      }
+    }
+    .header_bottom{
+      overflow: hidden;
+      padding:0.2rem 0.4rem;
+      border-bottom: 0.5px solid #bbb;
+      div.header_bottom_item{
+        float:left;
+        width:33%;
+        heigth:43px;
+        line-height: 36px;
+        font-size: 80%;
+      }
+    }
+  }
+</style>
--- src/page/center.vue
+++ src/page/center.vue
@@ -1,15 +1,94 @@
 <template>
     <div id="center">
-      <!--<Metabbar></Metabbar>-->
+      <blackHeader showHeader_do='false' word="个人中心"></blackHeader>
+      <div class="userHeader">
+        <img src="../assets/images/570ef05dNc02e5acf.jpg" alt="">
+        <p>我的名字</p>
+        <div class="pro_type_warp">
+          <div class="pro_type_warp_item">
+            <div class="pro_type_warp_img_box">
+              <div class="pro_type_warp_img"></div>
+              <div class="red_dd"></div>
+            </div>
+            <p>代付款</p>
+          </div>
+          <div class="pro_type_warp_item">
+            <div class="pro_type_warp_img_box">
+              <div class="pro_type_warp_img"></div>
+              <div class="red_dd"></div>
+            </div>
+            <p>待收货</p>
+          </div>
+          <div class="pro_type_warp_item">
+            <div class="pro_type_warp_img_box">
+              <div class="pro_type_warp_img"></div>
+              <div class="red_dd"></div>
+            </div>
+            <p>已完成</p>
+          </div>
+        </div>
+      </div>
+      <div class="menu_item_wrap first">
+        <a class="menu_item">
+          <em class="user_icon menu_item_icon menu_item_icon_order"></em>
+          我的订单
+          <em class="user_icon menu_item_jiantou"></em>
+        </a>
+        <a class="menu_item">
+          <em class="user_icon menu_item_icon menu_item_icon_order"></em>
+          我的优惠券
+          <em class="user_icon menu_item_jiantou"></em>
+        </a>
+        <a class="menu_item">
+          <em class="user_icon menu_item_icon menu_item_icon_order"></em>
+          我的预约
+          <em class="user_icon menu_item_jiantou"></em>
+        </a>
+        <a class="menu_item">
+          <em class="user_icon menu_item_icon menu_item_icon_order"></em>
+          我的喜欢
+          <em class="user_icon menu_item_jiantou"></em>
+        </a>
+        <a class="menu_item">
+          <em class="user_icon menu_item_icon menu_item_icon_order"></em>
+          收货地址
+          <em class="user_icon menu_item_jiantou"></em>
+        </a>
+        <a class="menu_item">
+          <em class="user_icon menu_item_icon menu_item_icon_order"></em>
+          退换货记录
+          <em class="user_icon menu_item_jiantou"></em>
+        </a>
+      </div>
+      <div class="menu_item_wrap second">
+        <a class="menu_item">
+          <em class="user_icon menu_item_icon menu_item_icon_order"></em>
+          用户服务协议
+          <em class="user_icon menu_item_jiantou"></em>
+        </a>
+        <a class="menu_item">
+          <em class="user_icon menu_item_icon menu_item_icon_order"></em>
+          帮助和售后
+          <em class="user_icon menu_item_jiantou"></em>
+        </a>
+        <a class="menu_item">
+          <em class="user_icon menu_item_icon menu_item_icon_order"></em>
+          关于我们
+          <em class="user_icon menu_item_jiantou"></em>
+        </a>
+      </div>
+      <div class="signOut">
+        退出登录
+      </div>
     </div>
 </template>
 
 <script>
-    import Metabbar from '../components/Metabbar/Metabbar.vue'
     import alert from '../components/alert/alert.vue'
+    import blackHeader from '../components/black-header/black-header.vue'
     export default {
         name:'center',
-        components:{Metabbar,alert},
+        components:{ alert,blackHeader },
         data(){
           return{
               showAlert:false,
@@ -37,7 +116,140 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
   #center{
-    height:100%;
-
+    background:#EEEEEE;
+    .userHeader{
+      padding:1rem 1rem 5rem 1rem;
+      width:100%;
+      position:relative;
+      background: url(../assets/images/header_bg.jpg) no-repeat center;
+      background-size: cover;
+      text-align:center;
+      img{
+        width:68px;
+        height:68px;
+        border-radius: 100px;
+      }
+      p{
+        margin-top:6px;
+        color:#fff;
+        font-size: 110%;
+      }
+      .pro_type_warp{
+        background: rgba(255,255,255,0.3);
+        width:100%;
+        overflow: hidden;
+        position: absolute;
+        bottom:0;
+        left:0;
+        .pro_type_warp_item{
+          float:left;
+          margin:0.5rem 0;
+          height:3rem;
+          width:33%;
+          font-size: 85%;
+          p{
+            font-size:95%;
+          }
+          .pro_type_warp_img{
+            width:30px;
+            height:25px;
+            margin:0 auto;
+            background: url(../assets/images/usercenter.png) no-repeat 0px -30px;
+            background-size: 30px;
+          }
+        }
+        .pro_type_warp_item:nth-child(2){
+          border-left:1px solid #fff;
+          border-right:1px solid #fff;
+          .pro_type_warp_img{
+            background: url(../assets/images/usercenter.png) no-repeat 0px 0px;
+            background-size: 30px;
+          }
+        }
+        .pro_type_warp_item:last-child{
+          .pro_type_warp_img{
+            background: url(../assets/images/usercenter.png) no-repeat 0px -60px;
+            background-size: 30px;
+          }
+        }
+      }
+    }
+    .menu_item_wrap{
+      margin: 10px 0;
+      background: #FFF;
+      font-size: 14px;
+      padding-left: 20px;
+      a{
+        display: block;
+        line-height: 50px;
+        height: 50px;
+        border-bottom:1px solid #ccc;
+        em.menu_item_icon_order{
+          background: url(../assets/images/usercenter.png) no-repeat;
+          background-size: 28px;
+          background-position: -5px -88px;
+          float: left;
+          margin-top: 15px;
+          display: block;
+          width: 28px;
+          height: 20px;
+        }
+        em.menu_item_jiantou{
+          background: url(../assets/images/usercenter.png) no-repeat;
+          float: right;
+          margin-top: 7px;
+          display: block;
+          width: 28px;
+          height: 30px;
+          background-size: 28px;
+          background-position: 0 -420px;
+        }
+      }
+    }
+    .menu_item_wrap.first{
+      a:nth-child(2) em.menu_item_icon_order{
+          background-position: -5px -116px;
+      }
+      a:nth-child(3) em.menu_item_icon_order{
+        background-position: -5px -144px;
+      }
+      a:nth-child(4) em.menu_item_icon_order{
+        background-position: -5px -172px;
+      }
+      a:nth-child(5) em.menu_item_icon_order{
+        background-position: -5px -200px;
+      }
+      a:nth-child(6) em.menu_item_icon_order{
+        background-position: -5px -228px;
+      }
+      a:nth-child(6){
+        border-bottom:none;
+      }
+    }
+    .menu_item_wrap.second{
+      a em.menu_item_icon_order{
+        background-position: -5px -256px;
+      }
+      a:nth-child(2) em.menu_item_icon_order{
+        background-position: -5px -284px;
+      }
+      a:nth-child(3) em.menu_item_icon_order{
+        background-position: -5px -312px;
+      }
+      a:nth-child(3){
+        border-bottom:none;
+      }
+    }
+    .signOut{
+      width:100%;
+      font-size: 80%;
+      display: block;
+      text-align: center;
+      line-height: 50px;
+      height: 50px;
+      margin: 10px 0;
+      color:#666;
+      background-color: #fff;
+    }
   }
 </style>
