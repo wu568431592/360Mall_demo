844db63537dc7fa697ef08ba7adfc6d1ca813f6d
wu568431592 <wuwenchao0314@163.com>
2017-06-19 16:35:08 +0800

    新增全部商品页面的一部分

--- src/page/allproduct.vue
+++ src/page/allproduct.vue
@@ -1,14 +1,42 @@
 <template>
     <div id="allproduct">
-      allproduct
+      <blackHeader showHeader_do='false' word="全部商品"></blackHeader>
+      <div class="titleList">
+        <ul @click="clickLi($event)">
+          <li class="left_title active">全部商品</li>
+          <li class="left_title">手机</li>
+          <li class="left_title">数码</li>
+          <li class="left_title">电脑周边</li>
+          <li class="left_title">家用电器</li>
+          <li class="left_title">汽车用品</li>
+          <li class="left_title">摄影摄像</li>
+          <li class="left_title">教育/文具</li>
+          <li class="left_title">户外/运动</li>
+        </ul>
+      </div>
+      <div class="proList">
+        <div class="sort_box">
+           <ul>
+             <li @click="changeSort($event)" class="sortItem green">默认</li>
+             <li @click="changeSort($event)" class="sortItem" id="price">价格<b class=""></b></li>
+             <li @click="changeSort($event)" class="sortItem">销量</li>
+             <li @click="changeSort($event)" class="sortItem" id="haspro"><span></span>仅看有货</li>
+           </ul>
+        </div>
+        <div class="main_box">
+          <ul>
+
+          </ul>
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
-    import Metabbar from '../components/Metabbar/Metabbar.vue'
+    import blackHeader from '../components/black-header/black-header.vue'
     export default {
         name:'allproduct',
-        components:{Metabbar},
+        components:{ blackHeader },
         data(){
           return {
             showAlert : false
@@ -18,7 +46,7 @@
           this.$store.state.isBarshow = true
         },
         mounted:function(){
-          this.changeTarBar(1)
+          this.changeTarBar(1);
         },
         methods:{
           changeTarBar(index){
@@ -26,6 +54,31 @@
             setTimeout(()=>{
               me[index].click();
             },100)
+          },
+          clickLi(e){
+              var me = e.target;
+              var list = document.getElementsByClassName('left_title');
+              for(var i = 0; i<list.length;i++){
+                  list[i].setAttribute('class','left_title');
+              }
+              me.setAttribute('class','left_title active');
+          },
+          changeSort(e){
+              var me = e.target;
+              if( me.tagName == 'LI'){
+                var list = document.getElementsByClassName('sortItem');
+                for(var i = 0; i<list.length;i++){
+                  list[i].setAttribute('class','sortItem');
+                }
+                me.setAttribute('class','sortItem green');
+              }else{
+                me = e.target.parentNode;
+                var list = document.getElementsByClassName('sortItem');
+                for(var i = 0; i<list.length;i++){
+                  list[i].setAttribute('class','sortItem');
+                }
+                me.setAttribute('class','sortItem green');
+              }
           }
         }
     }
@@ -36,5 +89,74 @@
   #allproduct{
     background:#fff;
     height:100%;
+    overflow: hidden;
+    .titleList{
+      float:left;
+      width:30%;
+      height:100%;
+      background:#F8F8F8;
+      ul li{
+        font-size:70%;
+        padding:10px 0 10px 15px;
+        border-left:2px solid transparent;
+      }
+      ul li.active{
+        background:#fff;
+        border-left:2px solid #82C92F;
+      }
+    }
+    .proList{
+      float:left;
+      width:70%;
+      .sort_box ul{
+        overflow: hidden;
+        font-size:75%;
+        width:100%;
+        li{
+          height:45px;
+          line-height: 25px;
+          float:left;
+          width:20%;
+          padding:10px 0;
+          text-align:center;
+          b{
+            display: inline-block;
+            vertical-align: middle;
+            width:7px;
+            height:15px;
+            margin-left:5px;
+            background: url(../assets/images/icon.png) no-repeat 0px 0px;
+            background-size: 100%;
+          }
+          b.up{
+            background: url(../assets/images/icon.png) no-repeat 0px -14px;
+            background-size: 100%;
+          }
+          b.down{
+            background: url(../assets/images/icon.png) no-repeat 0px -28px;
+            background-size: 100%;
+          }
+          span{
+            vertical-align: middle;
+            display: inline-block;
+            width:20px;
+            height:20px;
+            margin-right:5px;
+            background: url(../assets/images/stock.png) no-repeat center;
+            background-size: 100%;
+          }
+        }
+        li.green{
+          color:#74B72C;
+          span{
+            background: url(../assets/images/hasstock.png) no-repeat center;
+            background-size: 100%;
+          }
+        }
+        li:last-child{
+          width:40%;
+        }
+      }
+    }
   }
 </style>
